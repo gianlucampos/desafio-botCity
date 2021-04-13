2332e0233525f826b696182f6cc911b7d2554694
gianlucampos <gianlucampos97@gmail.com>
2021-04-13 20:14:02 -0300

    Resolvendo problema 1

--- src/main/java/dev/botcity/recrutamento_problems/problem1/Cte.java
+++ src/main/java/dev/botcity/recrutamento_problems/problem1/Cte.java
@@ -1,35 +1,42 @@
 package dev.botcity.recrutamento_problems.problem1;
 
 public class Cte {
-		
-	private String 	chave;
-	private String 	emissao;
-	private Integer serie;
-	private Double	valor;
-	
-	public String getChave() {
-		return chave;
-	}
-	public void setChave(String chave) {
-		this.chave = chave;
-	}
-	public String getEmissao() {
-		return emissao;
-	}
-	public void setEmissao(String emissao) {
-		this.emissao = emissao;
-	}
-	public Integer getSerie() {
-		return serie;
-	}
-	public void setSerie(Integer serie) {
-		this.serie = serie;
-	}
-	public Double getValor() {
-		return valor;
-	}
-	public void setValor(Double value) {
-		this.valor = value;
-	}
-	
+
+    private String chave;
+    private String emissao;
+    private Integer serie;
+    private Double valor;
+
+    public String getChave() {
+        return chave;
+    }
+
+    public void setChave(String chave) {
+        this.chave = chave;
+    }
+
+    public String getEmissao() {
+        return emissao;
+    }
+
+    public void setEmissao(String emissao) {
+        this.emissao = emissao;
+    }
+
+    public Integer getSerie() {
+        return serie;
+    }
+
+    public void setSerie(Integer serie) {
+        this.serie = serie;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double value) {
+        this.valor = value;
+    }
+
 }
--- src/main/java/dev/botcity/recrutamento_problems/problem1/Problem1.java
+++ src/main/java/dev/botcity/recrutamento_problems/problem1/Problem1.java
@@ -1,16 +1,28 @@
 package dev.botcity.recrutamento_problems.problem1;
 
+import dev.botcity.recrutamento_problems.problem2.Problem2;
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 /**
- *  1. Carregar o arquivo cte.xml localizado na pasta ./res/.
- *  2. Criar um objeto Cte (Conhecimento de Transporte).
- *  3. Preencher os campos, sabendo os seguintes caminhos na estrutura XML:
- *              
- *  - serie:   <cteProc><CTe><infCte><ide><serie>
- *  - chave:   <cteProc><CTe><infCte Id=>
- *  - emissao: <cteProc><CTe><infCte><ide><dhEmi>
- *  - valor: <cteProc><CTe><infCte><vPrest><vTPrest>
+ * 1. Carregar o arquivo cte.xml localizado na pasta ./res/. 2. Criar um objeto
+ * Cte (Conhecimento de Transporte). 3. Preencher os campos, sabendo os
+ * seguintes caminhos na estrutura XML:
+ *
+ * - serie:   <cteProc><CTe><infCte><ide><serie>
+ * - chave:   <cteProc><CTe><infCte Id=>
+ * - emissao: <cteProc><CTe><infCte><ide><dhEmi>
+ * - valor:   <cteProc><CTe><infCte><vPrest><vTPrest>
  *
  */
 public class Problem1 {
@@ -19,6 +31,40 @@ public class Problem1 {
      * Implementar metodo abaixo
      */
     private Cte loadCteFromXML(File file) {
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(file);
+            XPath xpath = XPathFactory.newInstance().newXPath();
+
+            //chave
+            XPathExpression expInfCte = xpath.compile("/cteProc/CTe/infCte");
+            NodeList nodesInfCte = (NodeList) expInfCte.evaluate(doc, XPathConstants.NODESET);
+            Element elemInfCte = (Element) nodesInfCte.item(0);
+            String chave = elemInfCte.getAttribute("Id");
+
+            //serie e emissao
+            XPathExpression expIde = xpath.compile("/cteProc/CTe/infCte/ide");
+            NodeList nodesIde = (NodeList) expIde.evaluate(doc, XPathConstants.NODESET);
+            Element elemIde = (Element) nodesIde.item(0);
+            String serie = elemIde.getElementsByTagName("serie").item(0).getTextContent();
+            String emissao = elemIde.getElementsByTagName("dhEmi").item(0).getTextContent();
+
+            //valor
+            XPathExpression expVprest = xpath.compile("/cteProc/CTe/infCte/vPrest");
+            NodeList nodesVprest = (NodeList) expVprest.evaluate(doc, XPathConstants.NODESET);
+            Element elemVprest = (Element) nodesVprest.item(0);
+            String valor = elemVprest.getElementsByTagName("vTPrest").item(0).getTextContent();
+
+            Cte cte = new Cte();
+            cte.setSerie(Integer.parseInt(serie));
+            cte.setChave(chave);
+            cte.setEmissao(emissao);
+            cte.setValor(Double.parseDouble(valor));
+            return cte;
+        } catch (Exception ex) {
+            Logger.getLogger(Problem2.class.getName()).log(Level.SEVERE, "Falha ao processar xml!", ex);
+        }
         return null;
     }
 
